411af8d60e946e851727e20ed99d6fe2d7807793
pajdak3 <pajdak3@github.com>
2015-11-20 13:10:53 +0000

    mouse fix

--- utils/handleMouse.js
+++ utils/handleMouse.js
@@ -5,17 +5,17 @@
 "use strict";
 
 var g_mouseX = 0;
-var mouseY = 0;
+var g_mouseY = 0;
 
 function handleMouse(evt) {
 	if (g_doMouse && !g_isUpdatePaused ) {
 		var oneGridHeight = g_canvas.height / 30;
 		var oneGridWidth = g_canvas.width / 20;
 		var shipRadius = entityManager.getShipRadius();
-		if (evt.clientX > oneGridWidth + shipRadius && evt.clientX < g_canvas.width - oneGridWidth - shipRadius)
-			g_mouseX = evt.clientX;
-		if (evt.clientY > oneGridHeight + shipRadius && evt.clientY < g_canvas.height - (2 * oneGridHeight) - shipRadius)
-			g_mouseY = evt.clientY;
+		if (evt.clientX > oneGridWidth + shipRadius && evt.clientX < g_canvas.width - oneGridWidth - 0.5*shipRadius)
+			g_mouseX = evt.clientX - 0.5*shipRadius;
+		if (evt.clientY > oneGridHeight + shipRadius && evt.clientY < g_canvas.height - (2 * oneGridHeight) - 0.5*shipRadius)
+			g_mouseY = evt.clientY - 0.5*shipRadius;
 		entityManager.setShip(g_mouseX, g_mouseY);
 	}
 }
